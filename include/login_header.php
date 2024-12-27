8559a7bb9443ae497ea232790c9b3e9c51071c1c
Bhumi-017 <cbhumi0107@gmail.com>
2024-12-27 08:23:58 +0530

    login header

--- include/login_header.php
+++ include/login_header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title><S></S>tudent time Management</title>
+  <title>Student Task Management</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
