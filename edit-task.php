527ada5961ae7451e76196de05332d4ab2465dd0
amrathakamath <amrathadkamath@gmail.com>
2024-12-29 20:49:13 +0530

    neww1

--- edit-task.php
+++ edit-task.php
@@ -27,6 +27,18 @@ include("include/sidebar.php");
 $sql = "SELECT * FROM task_info WHERE task_id='$task_id' ";
 $info = $obj_admin->manage_all_info($sql);
 $row = $info->fetch(PDO::FETCH_ASSOC);
+$deadline = $row['t_end_time'];
+
+date_default_timezone_set('Asia/Kolkata');
+
+$deadlineDateTime = new DateTime($deadline);
+$currentDateTime = new DateTime('now');
+$currentDateTime->setTimezone(new DateTimeZone('Asia/Kolkata'));
+
+
+// Convert to timestamps for comparison
+$unix_deadline = $deadlineDateTime->getTimestamp();
+$unix_current_time = $currentDateTime->getTimestamp();
 
 ?>
 
@@ -35,90 +47,91 @@ $row = $info->fetch(PDO::FETCH_ASSOC);
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
 
-    <div class="row">
-      <div class="col-md-12">
-        <div class="well well-custom">
-          <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-              <div class="well">
-                <h3 class="text-center bg-primary" style="padding: 7px;">Edit Task </h3><br>
+<div class="row">
+	<div class="col-md-12">
+	<div class="well well-custom">
+		<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="well">
+			<h3 class="text-center bg-primary" style="padding: 7px;">Edit Task </h3><br>
 
-                      <div class="row">
-                        <div class="col-md-12">
-                          <form class="form-horizontal" role="form" action="" method="post" autocomplete="off">
-                            <div class="form-group">
-			                    <label class="control-label col-sm-5">Task Title</label>
-			                    <div class="col-sm-7">
-			                      <input type="text" placeholder="Task Title" id="task_title" name="task_title" list="expense" class="form-control" value="<?php echo $row['t_title']; ?>" <?php if($user_role != 1){ ?> readonly <?php } ?> val required>
-			                    </div>
-			                  </div>
-			                  <div class="form-group">
-			                    <label class="control-label col-sm-5">Task Description</label>
-			                    <div class="col-sm-7">
-			                      <textarea name="task_description" id="task_description" placeholder="Text Deskcription" class="form-control" rows="5" cols="5"><?php echo $row['t_description']; ?></textarea>
-			                    </div>
-			                  </div>
-			                  <div class="form-group">
-			                    <label class="control-label col-sm-5">Start Time</label>
-			                    <div class="col-sm-7">
-			                      <input type="text" name="t_start_time" id="t_start_time"  class="form-control" value="<?php echo $row['t_start_time']; ?>" <?php if($user_role != 1){ ?> readonly <?php } ?> val required>
-			                    </div>
-			                  </div>
-			                  <div class="form-group">
-			                    <label class="control-label col-sm-5">End Time</label>
-			                    <div class="col-sm-7">
-			                      <input type="text" name="t_end_time" id="t_end_time" class="form-control" value="<?php echo $row['t_end_time']; ?>">
-			                    </div>
-			                  </div>
+					<div class="row">
+					<div class="col-md-12">
+						<form class="form-horizontal" role="form" action="" method="post" autocomplete="off">
+						<div class="form-group">
+							<label class="control-label col-sm-5">Task Title</label>
+							<div class="col-sm-7">
+								<input type="text" placeholder="Task Title" id="task_title" name="task_title" list="expense" class="form-control" value="<?php echo $row['t_title']; ?>" <?php if($user_role != 1){ ?> readonly <?php } ?> val required>
+							</div>
+							</div>
+							<div class="form-group">
+							<label class="control-label col-sm-5">Task Description</label>
+							<div class="col-sm-7">
+								<textarea name="task_description" id="task_description" placeholder="Text Deskcription" class="form-control" rows="5" cols="5"><?php echo $row['t_description']; ?></textarea>
+							</div>
+							</div>
+							<div class="form-group">
+							<label class="control-label col-sm-5">Start Time</label>
+							<div class="col-sm-7">
+								<input type="text" name="t_start_time" id="t_start_time"  class="form-control" value="<?php echo $row['t_start_time']; ?>" <?php if($user_role != 1){ ?> readonly <?php } ?> val required>
+							</div>
+							</div>
+							<div class="form-group">
+							<label class="control-label col-sm-5">End Time</label>
+							<div class="col-sm-7">
+								<input type="text" name="t_end_time" id="t_end_time" class="form-control" value="<?php echo $row['t_end_time']; ?>">
+							</div>
+							</div>
 
-			                  <div class="form-group">
-			                    <label class="control-label col-sm-5">Assign To</label>
-			                    <div class="col-sm-7">
-			                      <?php 
-			                        $sql = "SELECT user_id, fullname FROM tbl_admin WHERE user_role = 2";
-			                        $info = $obj_admin->manage_all_info($sql);   
-			                      ?>
-			                      <select class="form-control" name="assign_to" id="aassign_to" <?php if($user_role != 1){ ?> disabled="true" <?php } ?>>
-			                        <option value="">Select</option>
+							<div class="form-group">
+							<label class="control-label col-sm-5">Assign To</label>
+							<div class="col-sm-7">
+								<?php 
+								$sql = "SELECT user_id, fullname FROM tbl_admin WHERE user_role = 2";
+								$info = $obj_admin->manage_all_info($sql);   
+								?>
+								<select class="form-control" name="assign_to" id="aassign_to" <?php if($user_role != 1){ ?> disabled="true" <?php } ?>>
+								<option value="">Select</option>
 
-			                        <?php while($rows = $info->fetch(PDO::FETCH_ASSOC)){ ?>
-			                        <option value="<?php echo $rows['user_id']; ?>" <?php
-			                        	if($rows['user_id'] == $row['t_user_id']){
-			                         ?> selected <?php } ?>><?php echo $rows['fullname']; ?></option>
-			                        <?php } ?>
-			                      </select>
-			                    </div>
-			                   
-			                  </div>
+								<?php while($rows = $info->fetch(PDO::FETCH_ASSOC)){ ?>
+								<option value="<?php echo $rows['user_id']; ?>" <?php
+									if($rows['user_id'] == $row['t_user_id']){
+									?> selected <?php } ?>><?php echo $rows['fullname']; ?></option>
+								<?php } ?>
+								</select>
+							</div>
+							
+							</div>
 
-			                   <div class="form-group">
-			                    <label class="control-label col-sm-5">Status</label>
-			                    <div class="col-sm-7">
-			                      <select class="form-control" name="status" id="status">
-			                      	<option value="0" <?php if($row['status'] == 0){ ?>selected <?php } ?>>Incomplete</option>
-			                      	<option value="1" <?php if($row['status'] == 1){ ?>selected <?php } ?>>In Progress</option>
-			                      	<option value="2" <?php if($row['status'] == 2){ ?>selected <?php } ?>>Completed</option>
-			                      </select>
-			                    </div>
-			                  </div>
-                            
-                            <div class="form-group">
-                            </div>
-                            <div class="form-group">
-                              <div class="col-sm-offset-3 col-sm-3">
-                                
-                              </div>
+							<div class="form-group">
+							<label class="control-label col-sm-5">Status</label>
+							<div class="col-sm-7">
+								<select class="form-control" name="status" id="status">
+								<option value="0" <?php if($row['status'] == 0){ ?>selected <?php } ?>>Incomplete</option>
+								<option value="1" <?php if($row['status'] == 1){ ?>selected <?php } ?>>In Progress</option>
+								<option value="2" <?php if($row['status'] == 2){ ?>selected <?php } ?>>Completed</option>
+								</select>
+							</div>
+							</div>
+						
+						<div class="form-group">
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-3 col-sm-3">
+							
+							</div>
 
-                              <div class="col-sm-3">
-                                <button type="submit" name="update_task_info" class="btn btn-success-custom">Update Now</button>
-                              </div>
-                            </div>
-                          </form> 
-                        </div>
-                      </div>
+							<div class="col-sm-3">
+							<button type="submit" name="update_task_info" class="btn btn-success-custom" id="updateButton" >Update Now</button>
 
-              </div>
-            </div>
+							</div>
+						</div>
+						</form> 
+					</div>
+					</div>
+
+			</div>
+		</div>
           </div>
 
         </div>
@@ -144,5 +157,14 @@ $row = $info->fetch(PDO::FETCH_ASSOC);
 
 include("include/footer.php");
 
+echo "<script>
+    var formattedCurrentTime = $unix_current_time; // PHP Unix timestamp for current time
+    var formattedDeadline = $unix_deadline; // PHP Unix timestamp for deadline
+    // Get the button element
+    var updateButton = document.getElementById('updateButton');
+    if (formattedCurrentTime > formattedDeadline) {
+	updateButton.disabled = true; // Disable the button if the deadline has passed
+}
+</script>";
 ?>
 
